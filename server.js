8e5d81f23ef7823bfa2b15cd6cfa9bea08d4e5b1
Yael Brown <yaelrbrown@gmail.com>
2019-09-06 22:41:54 -0500

    Able to connect to db and manually add items to collection.

--- server.js
+++ server.js
@@ -1,7 +1,46 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path')
+const path = require('path');
+const mongoose = require("mongoose");
+
+// connect to db
+mongoose.connect("mongodb://app:password123@ds219078.mlab.com:19078/yaelspace", {
+  useNewUrlParser: true
+}, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("connected to database");
+  }
+});
+
+let loginSchema = new mongoose.Schema({
+  username: String,
+  firstname: String,
+  lastname: String,
+  password: String,
+  email: String
+});
+
+let login = mongoose.model("login", loginSchema);
+
+login.create({
+  username: "yaelTest",
+  firstname: "Yael",
+  lastname: "test",
+  password: "test123",
+  email: "yael@test.com"
+}, (error, data) => {
+  if (error) {
+    console.log("Problem logging document to the collection");
+    console.log(data);
+  } else {
+    console.log("Added Data to Collection");
+    console.log(data);
+  }
+});
+
 
 // Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname,'index.html')))
