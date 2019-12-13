f2f4aa2d79e7eb4d18d65a13247b2ecd142fe62c
Yael Brown <yaelrbrown@gmail.com>
2019-12-13 17:51:18 -0600

    moved db connection from server.js to db/db.js

--- server.js
+++ server.js
@@ -5,15 +5,9 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const port = 8080
 
-// Connect db
-const mysql = require('mysql');
+const db = require('./db/db');
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "codeup",
-  database: "ys"
-});
+// Connect db
 
 db.connect((err) => {
   if (err) {
@@ -26,7 +20,6 @@ db.connect((err) => {
 // Setup middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
