abb2aff58c4d572cc1182eec0ebec48f680a5a5f
Yael Brown <yaelrbrown@gmail.com>
2019-12-13 20:24:23 -0600

    added react-router-dom

--- ys/src/components/login/Login.js
+++ ys/src/components/login/Login.js
@@ -10,6 +10,8 @@ export default class Login extends Component {
           <input type="text" id="username" name="username" placeholder="login" required></input><br/>
           <input type="password" id="password" name="password" placeholder="password" required></input><br/>
           <button type="submit">Login</button>
+          <br/>
+          <p>Not a user? <a href="/register">Click here</a> to register.</p>
         </form>
       </div>
     )
