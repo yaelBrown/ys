6d650eb670bcae921f77c8e9a05714051697b9cc
Yael Brown <yaelrbrown@gmail.com>
2019-12-14 21:13:18 -0600

    Renamed switch.js to router.js and moved to src/

--- ys/src/App.js
+++ ys/src/App.js
@@ -3,17 +3,17 @@ import './css/App.css';
 import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from './components/nav/Navbar';
-import Login from './components/login/Login';
-import Index from './components/index/Index';
-import Register from './components/register/Register';
-import Switch from './components/Switch';
+// import Login from './components/login/Login';
+// import Index from './components/index/Index';
+// import Register from './components/register/Register';
+import Router from './Router';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar/>
-        <Switch/>
+        <Router/>
       </div>
     </BrowserRouter>
   );
---
+++ ys/src/Router.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Switch, Route } from "react-router-dom";
+import Register from './components/register/Register';
+import Index from './components/index/Index';
+import Login from './components/login/Login';
+
+export default class Router extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact strict path="/login" component={Login} />
+        <Route exact strict path="/register" component={Register} />
+        <Route exact strict path="/" exact component={Index} />
+      </Switch>
+    )
+  }
+}
