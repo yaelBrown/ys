cbd0760ffb081c2c9da3699a24f0fa23a72a4f6b
Yael Brown <yaelrbrown@gmail.com>
2019-12-15 14:53:43 -0600

    finished reg form, and to register new users on backend

--- ys/src/components/login/Login.js
+++ ys/src/components/login/Login.js
@@ -5,7 +5,7 @@ export default class Login extends Component {
   render() {
     return (
       <div className="login-form">
-        <form action="http://localhost:8080/login" method="post">
+        <form action="http://localhost:8080/api/users/login" method="post">
           <h1>Login</h1>
           <input type="email" id="username" name="username" placeholder="email@address.com" required></input><br/>
           <input type="password" id="password" name="password" placeholder="password" required></input><br/>
--- ys/src/components/register/Register.js
+++ ys/src/components/register/Register.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactFilestack from 'filestack-react';
+
+import './register.css';
 
 export default class Register extends Component {
   render() {
     return (
-      <div>
-        <h1>Register a new user</h1>
+      <div className="register-form">
+        <form action="http://localhost:8080/api/users/register" method="post">
+        {/* <form action="https://request-inspector.glitch.me/" method="post"> */}
+          <h1>Register a new user</h1>
+          <input type="text" id="fName" name="fName" placeholder="First Name"/>
+          <input type="text" id="lName" name="lName" placeholder="Last Name"/>
+          <br/>
+          <input type="email" id="email" name="email" placeholder="email@address.com" required/>
+          <br/>
+          <input type="password" id="password" name="password" placeholder="password" required/>
+          <input type="text" id="location" name="location" placeholder="city, state"/>
+          <h4>Birthday</h4>
+          <input type="date" id="birth_day" name="birth_day"/>
+          <br/>
+          <h4>Upload picture</h4>
+          <ReactFilestack
+            apikey={"A5FzQeyKZRbyNUEoo7QMnz"}
+            onSuccess={(res) => res}
+          />
+          <br/>
+          <small>(You can upload a picture later)</small>
+          <br/>
+          <button type="submit" id="submit">Register</button>
+          <button type="reset" id="reset">Reset</button>
+        </form>
       </div>
     )
   }
